c5559f0bf35689be78f9d2ce77eb0d297ab03d31
Jesse De Jong <jdoceau@gmail.com>
2022-04-29 10:44:43 +1000

    fixed issue with ul/li not building correctly, refactored createCard to outsource repeating code

--- js/dom.js
+++ js/dom.js
@@ -6,40 +6,52 @@ const form = document.getElementById("form");
 const results = document.getElementById("results");
 const searchButton = document.getElementById("search");
 
+// HTML constructor function
 const createCard = (image, title, authors, description) => {
-    // Section to contain each card
+    // Instantiate a container
     const outerDiv = document.createElement("section");
-    // Image of book cover, appended to section        
+
+    // IMAGE containing the book cover        
     const imageElement = document.createElement("img");
         imageElement.setAttribute("src", image);
         outerDiv.appendChild(imageElement);
-    // Heading for title, appended to section
-    const titleElement = document.createElement("h2");
-    const titleTextNode = document.createTextNode(title);
-        titleElement.appendChild(titleTextNode);
-        outerDiv.appendChild(titleElement);
-    // List for authors, appended to section
+
+    // TITLE containing the books title
+    elementCreator("h2", title, outerDiv);
+    // const titleElement = document.createElement("h2");
+    // const titleTextNode = document.createTextNode(title);
+    //     titleElement.appendChild(titleTextNode);
+    //     outerDiv.appendChild(titleElement);
+
+    // AUTHORS containing a list of all authors
     const listElement = document.createElement("ul");
-        const listItemElement = document.createElement("li");
-        const listItemTextNodes = authors.map( (author) => document.createTextNode(author) );
-        listItemTextNodes.map( (textNode) => listItemElement.appendChild(textNode) );
-        listElement.appendChild(listItemElement);
+        // MAP authors array and append an LI for each to the UL
+        authors.map( (author) => {
+            elementCreator("li", author, listElement);
+            // const listItemElement = document.createElement("li");
+            // const listItemTextNode = document.createTextNode(author);
+            // listItemElement.appendChild(listItemTextNode);
+            // listElement.appendChild(listItemElement);
+        })
         outerDiv.appendChild(listElement);
-    // Paragraph for description, appended to section
-    const paragraphElement = document.createElement("p");
-    const paragraphTextNode = document.createTextNode(description);
-        paragraphElement.appendChild(paragraphTextNode);
-        outerDiv.appendChild(paragraphElement);
+
+    // DESCRIPTION containing the provided description
+    elementCreator("p", description, outerDiv);
+    // const paragraphElement = document.createElement("p");
+    // const paragraphTextNode = document.createTextNode(description);
+    //     paragraphElement.appendChild(paragraphTextNode);
+    //     outerDiv.appendChild(paragraphElement);
+
     // Append completed Card to the DOM
     results.appendChild(outerDiv);
 };
 
-// const createElement = (type, text, parent) => {
-//     const newElement = document.createElement(type);
-//     const textNode = document.createTextNode(text);
-//     newElement.appendChild(textNode);
-//     parent.appendChild(newElement);
-// };
+const elementCreator = (type, text, parent) => {
+    const newElement = document.createElement(type);
+    const textNode = document.createTextNode(text);
+    newElement.appendChild(textNode);
+    parent.appendChild(newElement);
+};
 
 const clearDom = (parent) => {
     while (parent.firstChild) {
